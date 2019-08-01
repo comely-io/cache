a3e5d591ea74c46e84d4f54027f7ed25349ed08c
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-08-01 22:33:00 +0500

    version

--- src/Cache.php
+++ src/Cache.php
@@ -30,9 +30,9 @@ use Comely\Cache\Store\Redis;
 class Cache implements CacheStoreInterface
 {
     /** string Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "1.0.10";
+    public const VERSION = "1.0.20";
     /** int Version (Major * 10000 + Minor * 100 + Release) */
-    public const VERSION_ID = 10010;
+    public const VERSION_ID = 10020;
 
     public const SERIALIZED_PREFIX = "~comelyCachedItem";
     public const PLAIN_STRING_MAX_LEN = 64;
